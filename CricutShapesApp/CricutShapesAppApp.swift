198cf436d970c74b6e2c06b304b37d7340dd3558
MikeBookPro <131799683+MikeBookPro@users.noreply.github.com>
2025-05-03 17:40:00 +1200

    Initial Commit

---
+++ CricutShapesApp/CricutShapesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricutShapesAppApp.swift
+//  CricutShapesApp
+//
+//  Created by Michael Thornton on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CricutShapesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
